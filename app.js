467074d9815f1818333720ace7b365220143ff34
Alejandra Bejarano Alarcon <alejandrabejarano6@gmail.com>
2021-04-07 11:11:40 +0200

    connection mongodb and sauce schema

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use((req, res) => {
-    res.json({ message: 'Your request was successful!' }); 
+mongoose.connect('mongodb+srv://lejitas:iHKhg53KH3VpGi10@cluster0.1qly4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('succefully connected to MongoDB Atlas');
+    })
+    .catch((error) => {
+        console.log('Unable to connect to MongoDB Atlas');
+        console.error(error);
+    });
+
+app.use((req, res, next) => {
+    res.setHeader('Acces-Control-Allow-Origin', '*');
+    res.setHeader('Acces-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
  });
 
+ app.use(express.json());
+
+
+
 module.exports = app;
+
+//MONGODB PW: iHKhg53KH3VpGi10
+//MONGODB CONNECTION: mongodb+srv://lejitas:<password>@cluster0.1qly4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
