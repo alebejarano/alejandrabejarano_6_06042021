453db8fe01abc7f5b14010b5af9f9804c50c7f4d
Alejandra Bejarano Alarcon <alejandrabejarano6@gmail.com>
2021-04-17 19:11:14 +0200

    refacto

--- controllers/user.js
+++ controllers/user.js
@@ -25,22 +25,29 @@ exports.signup = (req, res, next) => {
 
 exports.login = (req, res, next) => {
   User.findOne({ email: maskedEmail(req.body.email) }).then((user) => {
+    //make sure that we have valid user credentials, check if the email given by the user is the one we have in our database
     if (!user) {
+      //the authentification failed
       return res.status(401).json({
         error: new Error('User not found!')
       });
     }
+    //the user exist, but we need to check the user's password
     bcrypt.compare(req.body.password, user.password).then((valid) => {
+      //comapre the password hashes
       if (!valid) {
+        //the password authentification failed
         return res.status(401).json({
           error: new Error('Incorrect password')
         });
       }
       const token = jwt.sign(
+        //the data we want to encode
         { userId: user._id }, 
         process.env.SECRET_TOKEN,
         { expiresIn: '24h'});
       res.status(200).json({
+        //the authentification is valid, passing our user id and the token
         userId: user._id,
         token: token
       });
--- middleware/auth.js
+++ middleware/auth.js
@@ -4,8 +4,10 @@ module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
+        //pass the token to be verify and the string that was use to be encoded
         const userId = decodedToken.userId;
         if (req.body.userId && req.body.userId !== userId) {
+            //check if the user id exist on our req and if it matches with the one from the token
             throw 'Invalid user Id'
         } else {
             next();
