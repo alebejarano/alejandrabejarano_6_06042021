fa67db5c1e0a2402aaab06e9dc5798ec91c0a0cc
Alejandra Bejarano Alarcon <alejandrabejarano6@gmail.com>
2021-04-21 16:50:45 +0200

    refacto

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,21 +1,22 @@
 const multer = require('multer');
 
 const MIME_TYPES = {
+    //allows to translate a MIME TYPE into an extension
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
     'image/png': 'png',
     'image/gif': 'gif'
 };
 
-//Wath to save and how to save our files
+//What to save and how to save our files
 const storage = multer.diskStorage({
-    //pass all of the options
+    //the options:
     destination: (req, file, callback) => {
         callback(null, 'images')
         //our callback receives 2 arguments: first is an errror, if there is, the second is the folder in which we want to save the file
     }, 
     filename: (req, file, callback) => {
-        // give the full file name: name + extention
+        // give the full file name: name + extension
         const name = file.originalname.split(' ').join('_');
         //to deal with the white space and to be replace by _
         const extension = MIME_TYPES[file.mimetype];
